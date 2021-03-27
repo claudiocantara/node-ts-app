a42b609c58710b30ae743d34e625958bf6755f7c
Claudio Alcantara <claudiomar.alcantara@gmail.com>
2021-03-27 11:11:14 -0300

    feat: add user controller base

---
+++ src/controllers/user/user.controller.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from 'express'
+import { UserModel } from './user.schema'
+
+export class UserController {
+  async list(req: Request, res: Response) {
+    const users = await UserModel.find()
+    return res.json(users).status(200)
+  }
+
+  async create(req: Request, res: Response) {
+    const { body } = req
+
+    try {
+      const user = await UserModel.create({
+        name: body.name,
+        birth_date: body.birth_date
+      })
+      if (!user) {
+        return res.json({ message: 'Something went wrong' }).status(400)
+      }
+
+      return res.json({ user }).status(200)
+    } catch (error) {
+      return res.json({ message: 'Something went wrong' }).status(400)
+    }
+  }
+}
